a684cf2feec7c95b9500b1dca3bb90e2b3275794
Zyco13 <bleronzekaj@icloud.com>
2026-05-10 20:51:37 +0200

    bug fix

--- src/main/java/com/bleron/portfolio_backend/ContactController.java
+++ src/main/java/com/bleron/portfolio_backend/ContactController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 })
 public class ContactController {
 
-    @Value("${resend.api.key}")
+    @Value("${RESEND_API_KEY:}")
     private String resendApiKey;
 
     @PostMapping
